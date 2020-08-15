cb84a331881f37d573b47a1ed86d276cc91c7b6e
Kevin Luk <khl7wh@virginia.edu>
2020-08-15 12:02:26 -0400

    Fixed port variable used for deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV != 'production') {
 }
 
 const API_KEY = process.env.API_KEY;
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const axios = require('axios');
 const { response } = require('express');
